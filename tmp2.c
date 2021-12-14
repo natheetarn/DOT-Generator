380cd1636111e5db8216ef6d617d82e6c0e47e39
natheetarn <natheetarn@gmail.com>
2021-12-14 14:00:19 +0700

    Added multi_graph_support

--- tmp2.c
+++ tmp2.c
@@ -2,6 +2,7 @@
 #include<string.h>
 #include<stdlib.h>
 #include<stdbool.h>
+#include<ctype.h>
 
 typedef struct graph{
     bool directed;
@@ -10,46 +11,11 @@ typedef struct graph{
     char **labels;
     int **matrix;
 }Graph;
-void read_num_node(FILE** f,struct graph* g,int index){
-    int tmp = 0;
-    fscanf(*f,"%d",&tmp);
-    g[index].num_node = tmp;
-    printf("Nodes: %d\n",tmp);
+int read_graph(FILE** f,struct graph* g,int index){
 
-}
-int read_adjacency_matrix(FILE** f,struct graph* g,int index){
-    g[index].matrix = (int**)malloc(g[index].num_node*sizeof(int*));// allocate array of int pointers
-    for(int i = 0; i < g[index].num_node; i++){
-        g[index].matrix[i] = (int*)malloc(g[index].num_node*sizeof(int));// allocate each pointers to int
-    } 
-
-    for(int i = 0 ; i < g[index].num_node; i++){
-        for(int j = 0; j < g[index].num_node; j++){
-            fscanf(*f,"%d",&g[index].matrix[i][j]);
-        }
-    }
-}
-void read_labels(FILE** f,struct graph* g,int index){
-    char buffer[101];
-    g[index].labels = (char**)malloc(g[index].num_node*sizeof(*g[index].labels));
-
-    for(int i = 0; i < g[index].num_node; i++){
-        fscanf(*f,"%100s",buffer);
-        g[index].labels[i] = (char*)malloc(strlen(buffer));
-        if(g[index].labels[i]){
-            strcpy(g[index].labels[i],buffer);
-        }
-    }
-
-    for (int i = 0; i < g[index].num_node; i++){
-        printf("Node %d's label: %s\n",i,g[index].labels[i]);
-    }
-}
-
-int read_graph_property(FILE** f,struct graph* g,int index){
+    //read property
     char tmp1,tmp2;
     fscanf(*f,"%c %c",&tmp1,&tmp2);
-    printf("%c %c\n",tmp1,tmp2);
     if(tmp1 == 'd' || tmp1 == 'D'){
         g[index].directed = true;
     }
@@ -67,12 +33,57 @@ int read_graph_property(FILE** f,struct graph* g,int index){
         printf("File format error.2");
         return -1;
     }
-    return 0;
+
+    //read num nodes
+    int tmp = 0;
+    fscanf(*f,"%d",&tmp);
+    g[index].num_node = tmp;
+    printf("Nodes: %d\n",tmp);
+
+    //read labels
+    char buffer[101];
+    g[index].labels = (char**)malloc(g[index].num_node*sizeof(*g[index].labels));
+
+    for(int i = 0; i < g[index].num_node; i++){
+        fscanf(*f,"%100s",buffer);
+        g[index].labels[i] = (char*)malloc(strlen(buffer));
+        if(g[index].labels[i]){
+            strcpy(g[index].labels[i],buffer);
+        }
+    }
+
+    for (int i = 0; i < g[index].num_node; i++){
+        printf("Node %d's label: %s\n",i,g[index].labels[i]);
+    }
+
+    //matrix
+    g[index].matrix = (int**)malloc(g[index].num_node*sizeof(int*));// allocate array of int pointers
+    for(int i = 0; i < g[index].num_node; i++){
+        g[index].matrix[i] = (int*)malloc(g[index].num_node*sizeof(int));// allocate each pointers to int
+    } 
+
+    for(int i = 0 ; i < g[index].num_node; i++){
+        for(int j = 0; j < g[index].num_node; j++){
+            fscanf(*f,"%d",&g[index].matrix[i][j]);
+            printf("%d ",g[index].matrix[i][j]);
+        }
+        printf("\n");
+    }
+    while(1){
+        int c;
+        c = fgetc(*f);
+        if(c == '\n' || c==EOF){
+            break;
+        }
+    }
+
+    
 }
 
+
 int check_argument(int argc){
-    if(argc != 3){
-        printf("Argument count error\nUsage: program_name file_name output_file_name");
+    if(argc != 4){
+        printf("Argument count error\nUsage: program_name file_name output_file_name number_of_graph");
         return -1;
     }
     return 0;
@@ -117,7 +128,7 @@ int write_to_file(struct graph* g, int index, char* out_file_name){
                 }
             }
         }
-        else if(g[index].weighted){
+        else if(!g[index].weighted){
             for(int i = 0; i < g[index].num_node ; i++){
                 for(int j = 0; j < g[index].num_node; j++){
                     if(g[index].matrix[i][j] == 0) continue;
@@ -129,6 +140,9 @@ int write_to_file(struct graph* g, int index, char* out_file_name){
         }
         fprintf(out_file,"}\n");
     }
+
+    
+    fclose(out_file);
 }
 int main(int argc, char *argv[]){
     
@@ -141,32 +155,29 @@ int main(int argc, char *argv[]){
         printf("Error opening file");
         return -1;
     }
-    int num_graph = 0;
-    fscanf(fp,"%d",&num_graph);
+    int num_graph = atoi(argv[3]);
     
 
     struct graph* graphs = (struct graph*)malloc(num_graph*sizeof(struct graph*));
 
     //read all graphs and store in graphs(dynamically allocated array of struct graph)
     for (int i = 0 ; i < num_graph ; i++ ){
-        if(read_graph_property(&fp,graphs,i)==-1){
-        return -1;
-    }
-    read_num_node(&fp,graphs,i);
-    read_labels(&fp,graphs,i);
-    read_adjacency_matrix(&fp,graphs,i);
+    read_graph(&fp,graphs,i);
     }
     
     // write each graph to new file
-    char out_name[20];
-    strcpy(out_name, argv[2]);
-    char tmp_name[20];
-    strcpy(tmp_name,out_name);
+    char tmp_name[50];
     for (int j = 0 ; j  < num_graph ; j++){
-        sprintf(tmp_name,"%s%d",tmp_name,j);
         
+        sprintf(tmp_name,"%s_%d.dot",argv[2],j);
+        printf("%s\n",tmp_name);
         write_to_file(graphs,j,tmp_name);
+        
     }
+
+    fclose(fp);
+    free(graphs);
+    
 
 
 
